eb8be0b099e7dab33b60bca7393fbb8cab339148
love-sonkar <loveboss806@gmail.com>
2023-07-04 20:34:40 +0530

    some more practice codes added

--- reversString.js
+++ reversString.js
@@ -11,3 +11,19 @@ function ReverseString(str) {
 function StringReverse(str) {
   return str.split("").reverse();
 }
+
+var arrayStringsAreEqual = function (word1, word2) {
+  let x = word1.toString().replace(/[,]/g, "");
+  let y = word2.toString().replace(/[,]/g, "");
+  if (x === y) return true;
+  return false;
+};
+
+// console.log(arrayStringsAreEqual(["abc", "d", "defg"], ["abcddefg"]));
+
+var lengthOfLastWord = function (s) {
+  let arr = s.trim().split(" ");
+  return arr[arr.length - 1].length;
+};
+
+console.log(lengthOfLastWord("hih alkjsd al  kajklsdf aseasdfsfai"));
