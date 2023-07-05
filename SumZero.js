b552de0e95cdfd1b84ff06d899688e8bdf1f0d7d
love-sonkar <loveboss806@gmail.com>
2023-07-05 22:29:23 +0530

    second method added

--- SumZero.js
+++ SumZero.js
@@ -2,15 +2,24 @@
 // input = [-5,-4,-3,-2,0,2,4,5,8 ] output = [ , ]
 
 let SumZero = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    for (let j = i + 1; j < array.length; j++) {
-      if (array[i] + array[j] == 0) {
-        return [array[i], array[j]];
+  //   for (let i = 0; i < array.length; i++) {
+  //     for (let j = i + 1; j < array.length; j++) {
+  //       if (array[i] + array[j] == 0) {
+  //         return [array[i], array[j]];
+  //       }
+  //     }
+  //   }
+  // second method
+  for (let numbers of array) {
+    for (let j in array) {
+      if (numbers + array[j] == 0) {
+        return [numbers, array[j]];
       }
     }
   }
+
   return [];
 };
 
-let result = SumZero([-5, -4, -3, 0, 2, 8]);
+let result = SumZero([-5, -4, -3, 0, 3, 2, 8]);
 console.log(result);
