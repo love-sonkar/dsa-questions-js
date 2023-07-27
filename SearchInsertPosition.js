32e2b5591bdf5f537894fdafee9639b7fb497edc
love-sonkar <loveboss806@gmail.com>
2023-07-27 23:59:25 +0530

    some changes

--- SearchInsertPosition.js
+++ SearchInsertPosition.js
@@ -1,6 +1,26 @@
 // 35. Search Insert Position
 // Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 
+var searchInsert = function (nums, target) {
+  let i = 0;
+  let j = nums.length - 1;
+  while (i <= j) {
+    let mid = Math.floor((i + j) / 2);
+    if (nums[mid] == target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      j = mid - 1;
+    } else {
+      i = mid + 1;
+    }
+  }
+  return i;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 2));
+
+// my old solution
+
 // var searchInsert = function (nums, target) {
 //   for (let i = 0; i < nums.length; i++) {
 //     if (nums[i] == target) return i;
@@ -17,8 +37,3 @@ var searchInsert = function (nums, target) {
 };
 
 console.log(searchInsert([], 7));
-
-// 136. Single Number
-// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
-
-// You must implement a solution with a linear runtime complexity and use only constant extra space.
