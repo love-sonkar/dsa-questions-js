94f9f03c8877cf39a07fd1a06b976f071a40f42e
love-sonkar <loveboss806@gmail.com>
2023-08-08 23:43:50 +0530

    added new solution

--- practice2.js
+++ practice2.js
@@ -1,8 +1,8 @@
 var countKDifference = function (nums, k) {
   let count = 0;
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.lengh; j++) {
-      if (i < j || nums[i] - nums[j] == k) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) == k) {
         count++;
       }
     }
